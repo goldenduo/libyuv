6d01b9d6e2ef0a37c2c6b6fbca4170bb74412291
Frank Barchard <fbarchard@google.com>
2025-02-08 21:05:36 -0800

    ARGBToUV Matrix for SSSE3
    Was C
    ARGBToI444_Opt (361 ms)
    ARGBToJ444_Opt (1564 ms)
    RAWToI444_Opt (400 ms)
    RAWToJ444_Opt (1706 ms)
    
    Now SSSE3
    ARGBToI444_Opt (357 ms)
    ARGBToJ444_Opt (346 ms)
    RAWToI444_Opt (404 ms)
    RAWToJ444_Opt (401 ms)
    
    Profile
        48.78%  ARGBToUVJ444Row_SSSE3
        28.82%  RAWToARGBRow_SSSE3
        18.48%  ARGBToYJRow_AVX2
    
    
    Bug: 381138208
    Change-Id: I696b2d83435bc985aa38df831e01ff1a658da56e

--- source/convert_from_argb.cc
+++ source/convert_from_argb.cc
@@ -60,6 +60,14 @@ int ARGBToI444(const uint8_t* src_argb,
     }
   }
 #endif
+#if defined(HAS_ARGBTOUV444ROW_AVX2)
+  if (TestCpuFlag(kCpuHasAVX2)) {
+    ARGBToUV444Row = ARGBToUV444Row_Any_AVX2;
+    if (IS_ALIGNED(width, 32)) {
+      ARGBToUV444Row = ARGBToUV444Row_AVX2;
+    }
+  }
+#endif
 #if defined(HAS_ARGBTOUV444ROW_NEON)
   if (TestCpuFlag(kCpuHasNEON)) {
     ARGBToUV444Row = ARGBToUV444Row_Any_NEON;
@@ -2442,6 +2450,14 @@ int ARGBToJ444(const uint8_t* src_argb,
     ARGBToUVJ444Row = ARGBToUVJ444Row_Any_SSSE3;
     if (IS_ALIGNED(width, 16)) {
       ARGBToUVJ444Row = ARGBToUVJ444Row_SSSE3;
+    }
+  }
+#endif
+#if defined(HAS_ARGBTOUVJ444ROW_AVX2)
+  if (TestCpuFlag(kCpuHasAVX2)) {
+    ARGBToUVJ444Row = ARGBToUVJ444Row_Any_AVX2;
+    if (IS_ALIGNED(width, 32)) {
+      ARGBToUVJ444Row = ARGBToUVJ444Row_AVX2;
     }
   }
 #endif
