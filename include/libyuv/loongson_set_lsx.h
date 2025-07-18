c746f3cede80eb6984451bbadb962b39c1989bfd
Xi Ruoyao <xry111@xry111.site>
2025-07-18 11:37:08 +0800

    loong64: Make the runtime CPU feature check for LSX/LASX optimization really useful
    In the code base the invocation of LSX/LASX optimized routines are
    already guarded by TestCpuFlag().  But the guard is not really useful
    yet, consider LASX for example:
    
    - If the downstream already passes -mlasx building libyuv, the LASX
      optimized routines are built, but the produced library won't run
      on any CPU lacking LASX (for example Loongson 2K3000) and the
      runtime checks would do nothing useful.  Thus we assume the
      downstream builder does not explicitly pass -mlasx, nor use a
      toolchain with LASX enabled by default.
    - With the GN build system, if libyuv_use_lasx is enabled, only
      source/row_lasx.cc is built with -mlasx and the other translation
      units (for example source/rotate.cc) are not.  And when those TU
      include headers like include/libyuv/row.h, the HAS_*_LASX macros
      are not defined (as -mlasx is not used building the TU and HAS_*_LASX
      would only be defined if __loongarch_asx).  So the LASX optimized
      routines are built but completely unused.
    - With the GYP build system, LASX optimized routines are not built
      at all (commit 6ecfe106c304d1eeaa31d1b29a42c58fb70d35ca enabled
      it by default, but it was reverted by the previous commit for
      hardware compatibility).
    - With the CMake build system, it passes -mlasx building all
      translation units as long as the compiler supports -mlasx.  The
      produced library won't run on any CPU lacking LASX, thus the runtime
      checking is pointless.  This behavior is also unfriendly and
      surprising to the downstream builders like what I commented for the
      earlier status of GYP in the previous commit message.
    - With linux.mk, the situation is like GYP: LASX is not enabled at all.
    
    As the runtime checkings already exist, the best way to take the
    advantage of LSX/LASX is just making them really useful (like on
    x86_64 and aarch64), instead of (silently or not) breaking the hardware
    compatibility.  To achieve the goal:
    
    - The HAS_*_L{,A}SX macros no longer requires __loongarch_{,a}sx.
    - We no longer rely on the build system to pass -ml{,a}sx.  Instead we
      use the target pragma supported by Clang and GCC >= 15.   GYP may also
      do a unified build, then a unified TU may include
      loongson_intrinsics.h twice, once in the LSX context and another time
      in the LASX context, now the #ifdef guard against duplicate inclusion
      will drop the LASX intrinsics.  So the header now uses three different
      duplicate inclusion guards to avoid that.  If the compiler is too old
      to support the pragma, LIBYUV_DISABLE_L{,A}SX are defined in
      cpu_support.h, similar to various compiler version checks for other
      architectures already existing there.
    
    Change-Id: I503674b42ab77b8b61190f2a7749dee2e246ed19

---
+++ include/libyuv/loongson_set_lsx.h
@@ -0,0 +1,32 @@
+/*
+ *  Copyright 2025 The LibYuv Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS. All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifdef __loongarch__
+
+#ifndef LSX_LASX_PUSHED
+#define LSX_LASX_PUSHED
+#else
+#error "loongson_set_l{a,}sx.h ... loongson_unset_target.h shouldn't nest"
+#endif
+
+#ifndef __loongarch_sx
+#define LSX_PUSHED
+#ifdef __clang__
+#pragma clang attribute push(__attribute__((target("lsx"))), apply_to=function)
+#define __loongarch_sx
+#elif defined(__GNUC__) && __GNUC__ >= 15
+#pragma GCC push_options
+#pragma GCC target "lsx"
+#else
+#error "LSX is requested but we cannot tell the compiler to enable it"
+#endif // __clang__
+#endif // __loongarch_sx
+
+#endif // __loongarch__
