57748f441f90326314c24fe894b1a03924db8275
Frank Barchard <fbarchard@google.com>
2025-10-15 17:07:07 -0700

    Deprecate MIPS and MSA support.
    - Remove *_msa.cc source files
    - Update build files
    - Update header references, planar ifdefs for row functions
    - Update documentation on supported platforms
    
    Bug: 434383432
    Change-Id: I072d6aac4956f0ed668e64614ac8557612171f76

--- source/cpu_id.cc
+++ source/cpu_id.cc
@@ -359,35 +359,6 @@ LIBYUV_API SAFEBUFFERS int RiscvCpuCaps(const char* cpuinfo_name) {
   return flag;
 }
 
-LIBYUV_API SAFEBUFFERS int MipsCpuCaps(const char* cpuinfo_name) {
-  char cpuinfo_line[512];
-  int flag = 0;
-  FILE* f = fopen(cpuinfo_name, "re");
-  if (!f) {
-    // Assume nothing if /proc/cpuinfo is unavailable.
-    // This will occur for Chrome sandbox for Pepper or Render process.
-    return 0;
-  }
-  memset(cpuinfo_line, 0, sizeof(cpuinfo_line));
-  while (fgets(cpuinfo_line, sizeof(cpuinfo_line), f)) {
-    if (memcmp(cpuinfo_line, "cpu model", 9) == 0) {
-      // Workaround early kernel without MSA in ASEs line.
-      if (strstr(cpuinfo_line, "Loongson-2K")) {
-        flag |= kCpuHasMSA;
-      }
-    }
-    if (memcmp(cpuinfo_line, "ASEs implemented", 16) == 0) {
-      if (strstr(cpuinfo_line, "msa")) {
-        flag |= kCpuHasMSA;
-      }
-      // ASEs is the last line, so we can break here.
-      break;
-    }
-  }
-  fclose(f);
-  return flag;
-}
-
 #if defined(__loongarch__) && defined(__linux__)
 // Define hwcap values ourselves: building with an old auxv header where these
 // hwcap values are not defined should not prevent features from being enabled.
@@ -461,10 +432,6 @@ static SAFEBUFFERS int GetCpuFlags(void) {
       }
     }
   }
-#endif
-#if defined(__mips__) && defined(__linux__)
-  cpu_info = MipsCpuCaps("/proc/cpuinfo");
-  cpu_info |= kCpuHasMIPS;
 #endif
 #if defined(__loongarch__) && defined(__linux__)
   cpu_info = LoongarchCpuCaps();
